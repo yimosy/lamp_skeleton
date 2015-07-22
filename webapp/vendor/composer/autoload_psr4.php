28bf6942df39a4af11e1773840f438d1c553ddef
yimosy <yimosy@gmail.com>
2015-07-22 14:35:35 +0900

    twig-bridge追加

--- webapp/vendor/composer/autoload_psr4.php
+++ webapp/vendor/composer/autoload_psr4.php
@@ -12,6 +12,7 @@ return array(
     'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Symfony\\Component\\Debug\\' => array($vendorDir . '/symfony/debug'),
+    'Symfony\\Bridge\\Twig\\' => array($vendorDir . '/symfony/twig-bridge'),
     'Silex\\' => array($vendorDir . '/silex/silex/src/Silex'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
 );
